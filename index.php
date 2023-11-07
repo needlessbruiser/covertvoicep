44c49e1e26af4c3bc58250c02f700b0e74246b95
Ahsan Ghaffar <115527661+needlessbruiser@users.noreply.github.com>
2023-11-07 11:22:37 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Delve Into Innovation with CovertVoice.com</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Delve Into Innovation with CovertVoice</h1>
+        <p>CovertVoice is your curated portal to the tech universe, offering exclusive insights into the latest electronic marvels and cutting-edge software. Join us on a voyage through technological wonders and keep pace with the evolving digital landscape.</p>
+        <p>To uncover more, make your way to <a href="https://covertvoice.com/">CovertVoice.com</a>.</p>
+
+        <div>
+        </div>
+        <!-- <a href="https://www.facebook.com/ahsanghaffar23/" target="_blank"><button>Connect on Facebook</button></a> -->
+
+    </div>
+</body>
+
+</html>
